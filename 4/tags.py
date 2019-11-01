816e11b4371e9dfed1af3e9733746964cf479add
realnitinworks <realnitinworks@gmail.com>
2019-11-01 09:02:54 +0530

    submission Bite 4 @ codechalleng.es

---
+++ 4/tags.py
@@ -0,0 +1,22 @@
+import os
+from collections import Counter
+import urllib.request
+import re
+
+# prep
+tempfile = os.path.join('/tmp', 'feed')
+urllib.request.urlretrieve('http://bit.ly/2zD8d8b', tempfile)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+# start coding
+
+def get_pybites_top_tags(n=10):
+    """use Counter to get the top 10 PyBites tags from the feed
+       data already loaded into the content variable"""
+    regex = re.compile(r"<category>(.*?)</category>")  # Non-greedy
+    tags = re.findall(regex, content)
+    return Counter(tags).most_common(n)
+    
