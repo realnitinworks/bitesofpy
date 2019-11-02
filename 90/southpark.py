bced155284f90bb9dd2bcedda1ac98ce4e99e8b8
realnitinworks <realnitinworks@gmail.com>
2019-11-02 17:33:18 +0530

    submission Bite 90 @ codechalleng.es

---
+++ 90/southpark.py
@@ -0,0 +1,31 @@
+from collections import Counter, defaultdict
+import csv
+
+import requests
+
+CSV_URL = 'https://raw.githubusercontent.com/pybites/SouthParkData/master/by-season/Season-{}.csv' # noqa E501
+
+
+def get_season_csv_file(season):
+    """Receives a season int, and downloads loads in its
+       corresponding CSV_URL"""
+    with requests.Session() as s:
+        download = s.get(CSV_URL.format(season))
+        return download.content.decode('utf-8')
+
+
+def get_num_words_spoken_by_character_per_episode(content):
+    """Receives loaded csv content (str) and returns a dict of
+       keys=characters and values=Counter object,
+       which is a mapping of episode=>words spoken"""
+    words = defaultdict(Counter)
+    reader = csv.DictReader(content.splitlines())
+    for row in reader:
+        character = row['Character']
+        episode = row['Episode']
+        word_count = len(row['Line'].split())
+        words[character][episode] += word_count
+
+    return words
+
+
