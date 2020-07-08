550c0d85e3a8b67064f4c5bab1a40917e53ff85e
realnitinworks <realnitinworks@gmail.com>
2020-07-08 08:05:49 +0530

    submission Bite 31 @ codechalleng.es

---
+++ 31/matmul.py
@@ -0,0 +1,40 @@
+class Matrix(object):
+
+    def __init__(self, values):
+        self.values = values
+
+    def __repr__(self):
+        return f'<Matrix values="{self.values}">'
+
+    def __len__(self):
+        return len(self.values)
+
+    def __matmul__(self, other):
+        result = [
+            [
+                0
+                for _ in range(len(other.values[0]))
+            ]
+            for _ in range(len(self))
+        ]
+
+        # Iterate through rows of self
+        for i in range(len(self)):
+            # Iterate through columns of other
+            for j in range(len(other.values[0])):
+                # Iterate through rows of other
+                for k in range(len(other.values)):
+                    result[i][j] += self.values[i][k] * other.values[k][j]
+
+        return Matrix(result)
+
+    def __rmatmul__(self, other):
+        return self @ other
+
+    def __imatmul__(self, other):
+        result_matrix = self @ other
+        for i in range(len(self)):
+            for j in range(len(self.values[0])):
+                self.values[i][j] = result_matrix.values[i][j]
+
+        return self
