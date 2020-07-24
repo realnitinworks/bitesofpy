ab44a43e9a8c74c007b348ad42ded6da6777c8e0
realnitinworks <realnitinworks@gmail.com>
2020-07-24 13:15:09 +0530

    submission Bite 175 @ codechalleng.es

---
+++ 175/missing_dates.py
@@ -0,0 +1,20 @@
+from dateutil.rrule import DAILY, rrule
+
+
+def get_missing_dates(dates):
+    """Receives a range of dates and returns a sequence
+       of missing datetime.date objects (no worries about order).
+
+       You can assume that the first and last date of the
+       range is always present (assumption made in tests).
+
+       See the Bite description and tests for example outputs.
+    """
+    min_date = min(dates)
+    max_date = max(dates)
+
+    return (
+        dt.date()
+        for dt in rrule(DAILY, dtstart=min_date, until=max_date)
+        if dt.date() not in dates
+    )
