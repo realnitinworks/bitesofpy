84af9a13871f087f659b87f8828fc0bce0796f17
realnitinworks <realnitinworks@gmail.com>
2020-07-25 09:35:51 +0530

    submission Bite 186 @ codechalleng.es

---
+++ 186/books.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from dateutil.parser import parse
+
+# work with a static date for tests, real use = datetime.now()
+NOW = datetime(2019, 3, 17, 16, 28, 42, 966663)
+WEEKS_PER_YEAR = 52
+
+
+def get_number_books_read(books_per_year_goal: int,
+                          at_date: str = None) -> int:
+    """Based on books_per_year_goal and at_date, return the
+       number of books that should have been read.
+       If books_per_year_goal negative or 0, or at_date is in the
+       past, raise a ValueError."""
+
+    at_date = at_date or str(NOW)
+    date = parse(at_date)
+
+    if books_per_year_goal <= 0:
+        raise ValueError("Goal must be greater than 0")
+
+    if date < NOW:
+        raise ValueError("Date must not be in the past")
+
+    _, at_week, _ = date.isocalendar()
+
+    return int(at_week/WEEKS_PER_YEAR * books_per_year_goal)
