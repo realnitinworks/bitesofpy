51507b9e3583f2f5610341de3ceb539d46ab9d31
realnitinworks <realnitinworks@gmail.com>
2020-08-19 15:50:35 +0530

    submission Bite 70 @ codechalleng.es

---
+++ 70/eggs.py
@@ -0,0 +1,18 @@
+from random import choice
+
+COLORS = 'red blue green yellow brown purple'.split()
+
+
+class EggCreator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.index < self.limit:
+            self.index += 1
+            return f"{choice(COLORS)} egg"
+        raise StopIteration
