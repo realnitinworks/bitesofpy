48e77d3d7933d12845d521177bab33a00a5ccc33
realnitinworks <realnitinworks@gmail.com>
2020-08-21 07:54:19 +0530

    submission Bite 78 @ codechalleng.es

---
+++ 78/common.py
@@ -0,0 +1,11 @@
+def common_languages(programmers):
+    """Receive a dict of keys -> names and values -> a sequence of
+       of programming languages, return the common languages"""
+
+    langs_iterator = iter(programmers.values())
+    common_languages = set(next(langs_iterator))  # Initialize with a value
+
+    for langs in langs_iterator:   # Loop over the next values
+        common_languages &= set(langs)  # set operation on the values
+
+    return common_languages
