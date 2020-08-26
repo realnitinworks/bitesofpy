8a0a92d370a2215b2d829ea190c86a50a85cd833
realnitinworks <realnitinworks@gmail.com>
2020-08-26 08:04:24 +0530

    submission Bite 100 @ codechalleng.es

---
+++ 100/tail.py
@@ -0,0 +1,9 @@
+def tail(filepath, n):
+    """Similate Unix' tail -n, read in filepath, parse it into a list,
+       strip newlines and return a list of the last n lines"""
+
+    with open(filepath) as f:
+        return [
+            line.strip()
+            for line in list(f)[-n:]
+        ]
