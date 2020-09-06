79b8abc91b2222c1d6b5add8fc518f1e2444181e
realnitinworks <realnitinworks@gmail.com>
2020-09-06 13:57:52 +0530

    submission Bite 153 @ codechalleng.es

---
+++ 153/rounding.py
@@ -0,0 +1,14 @@
+import math
+
+
+def round_up_or_down(transactions, up=True):
+    """Round the list of transactions passed in.
+       If up=True (default) round up, else round down.
+       Return a new list of rounded values
+    """
+    func = math.ceil if up else math.floor
+
+    return [
+        func(number)
+        for number in transactions
+    ]
