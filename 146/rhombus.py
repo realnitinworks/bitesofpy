e891b72283e8dcc1f37743bbfa0a00487386b169
realnitinworks <realnitinworks@gmail.com>
2020-09-06 12:39:34 +0530

    submission Bite 146 @ codechalleng.es

---
+++ 146/rhombus.py
@@ -0,0 +1,27 @@
+STAR = '*'
+
+
+def gen_rhombus(width):
+    """Create a generator that yields the rows of a rhombus row
+       by row. So if width = 5 it should generate the following
+       rows one by one:
+
+       gen = gen_rhombus(5)
+       for row in gen:
+           print(row)
+
+        output:
+          *
+         ***
+        *****
+         ***
+          *
+    """
+    rhombus = [
+        range(1, width + 1, 2),  # Upper part of rhombus
+        range(width - 2, 0, -2)  # Lower part of rhombus
+    ]
+
+    for part in rhombus:
+        for n in part:
+            yield f"{STAR * n:^{width}}"  # :^ => aligns center
