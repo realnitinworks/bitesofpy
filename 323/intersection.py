08220cde92d7eea8984df0887740310324823d7d
Nitin George Cherian <realnitinworks@gmail.com>
2021-01-19 14:44:45 +0530

    submission Bite 323 @ codechalleng.es

---
+++ 323/intersection.py
@@ -0,0 +1,15 @@
+import functools
+from typing import Iterable, Set, Any
+
+
+def intersection(*args: Iterable) -> Set[Any]:
+    args = (arg for arg in args if arg)
+    try:
+        result = set(next(args))
+    except StopIteration:
+        return set()
+
+    for arg in args:
+        result = result.intersection(arg)
+    return result
+
